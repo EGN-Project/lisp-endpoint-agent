0b14153f944c40fa537d92128746bfbc1c6bf49b
--unset <soto.carlodavid@gmail.com>
2024-04-10 16:55:34 -0400

    Application gateway now functioning

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -1,27 +1,10 @@
-import * as grpc from "@grpc/grpc-js";
-import express from "express";
-import {
-  connect,
-  Contract,
-  Identity,
-  Signer,
-  signers,
-} from "@hyperledger/fabric-gateway";
-import * as crypto from "crypto";
-import { promises as fs } from "fs";
-import * as path from "path";
-import { TextDecoder } from "util";
-import assert from "assert";
-
-// Load environment variables from a .env file if present.
-require("dotenv").config();
-
-/**
- * envOrDefault() will return the value of an environment variable, or a default value if the variable is undefined.
- */
- function envOrDefault(key: string, defaultValue: string): string {
-  return process.env[key] || defaultValue;
-}
+import * as grpc from '@grpc/grpc-js';
+import { connect, Contract, Identity, Signer, signers } from '@hyperledger/fabric-gateway';
+import * as crypto from 'crypto';
+import { promises as fs } from 'fs';
+import * as path from 'path';
+import { TextDecoder } from 'util';
+import express from 'express';
 
 const channelName = envOrDefault('CHANNEL_NAME', 'mychannel');
 const chaincodeName = envOrDefault('CHAINCODE_NAME', 'basic');
@@ -45,39 +28,37 @@ const peerEndpoint = envOrDefault('PEER_ENDPOINT', 'localhost:7051');
 // Gateway peer SSL host name override.
 const peerHostAlias = envOrDefault('PEER_HOST_ALIAS', 'peer0.org1.example.com');
 
+const utf8Decoder = new TextDecoder();
 const app = express();
 const port = 3000;
 
 
-
-const utf8Decoder = new TextDecoder();
 const deploymentID = `deployment${Date.now()}`;
 const revocationID = `revocation${Date.now()}`;
 
 async function main(): Promise<void> {
   await displayInputParameters();
 
-  // The gRPC client connection should be shared by
-  // all Gateway connections to this endpoint.
+  // The gRPC client connection should be shared by all Gateway connections to this endpoint.
   const client = await newGrpcConnection();
 
   const gateway = connect({
-    client,
-    identity: await newIdentity(),
-    signer: await newSigner(),
-    // Default timeouts for different gRPC calls
-    evaluateOptions: () => {
-      return { deadline: Date.now() + 5000 }; // 5 seconds
-    },
-    endorseOptions: () => {
-      return { deadline: Date.now() + 15000 }; // 15 seconds
-    },
-    submitOptions: () => {
-      return { deadline: Date.now() + 5000 }; // 5 seconds
-    },
-    commitStatusOptions: () => {
-      return { deadline: Date.now() + 60000 }; // 1 minute
-    },
+      client,
+      identity: await newIdentity(),
+      signer: await newSigner(),
+      // Default timeouts for different gRPC calls
+      evaluateOptions: () => {
+          return { deadline: Date.now() + 5000 }; // 5 seconds
+      },
+      endorseOptions: () => {
+          return { deadline: Date.now() + 15000 }; // 15 seconds
+      },
+      submitOptions: () => {
+          return { deadline: Date.now() + 5000 }; // 5 seconds
+      },
+      commitStatusOptions: () => {
+          return { deadline: Date.now() + 60000 }; // 1 minute
+      },
   });
 
   try {
@@ -283,23 +264,17 @@ async function main(): Promise<void> {
   }
 }
 
-main().catch((error) => {
-  console.error("******** FAILED to run the application:", error);
+main().catch(error => {
+  console.error('******** FAILED to run the application:', error);
   process.exitCode = 1;
 });
 
 async function newGrpcConnection(): Promise<grpc.Client> {
   const tlsRootCert = await fs.readFile(tlsCertPath);
   const tlsCredentials = grpc.credentials.createSsl(tlsRootCert);
-
   return new grpc.Client(peerEndpoint, tlsCredentials, {
-    "grpc.ssl_target_name_override": peerHostAlias,
+      'grpc.ssl_target_name_override': peerHostAlias,
   });
-}
-
-async function getFirstDirFileName(dirPath: string): Promise<string> {
-  const files = await fs.readdir(dirPath);
-  return path.join(dirPath, files[0]);
 }
 
 async function newIdentity(): Promise<Identity> {
@@ -308,20 +283,15 @@ async function newIdentity(): Promise<Identity> {
   return { mspId, credentials };
 }
 
+async function getFirstDirFileName(dirPath: string): Promise<string> {
+  const files = await fs.readdir(dirPath);
+  return path.join(dirPath, files[0]);
+}
+
 async function newSigner(): Promise<Signer> {
-  // Ignore non-key files in the key directory, e.g. `.DS_Store`.
-  const files = (await fs.readdir(keyDirectoryPath)).filter((fileName) =>
-    fileName.endsWith(".key")
-  );
-
-  const keyFilename = files.at(0);
-
-  assert(keyFilename !== undefined, "No key file found in the keys directory");
-
-  const keyPath = path.resolve(keyDirectoryPath, keyFilename);
+  const keyPath = await getFirstDirFileName(keyDirectoryPath);
   const privateKeyPem = await fs.readFile(keyPath);
   const privateKey = crypto.createPrivateKey(privateKeyPem);
-
   return signers.newPrivateKeySigner(privateKey);
 }
 
@@ -329,15 +299,14 @@ async function newSigner(): Promise<Signer> {
  * This type of transaction would typically only be run once by an application the first time it was started after its
  * initial deployment. A new version of the chaincode deployed later would likely not need to run an "init" function.
  */
-async function initLedger(contract: Contract): Promise<void> {
-  console.log(
-    "\n--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger"
-  );
+ async function initLedger(contract: Contract): Promise<void> {
+  console.log('\n--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger');
 
-  await contract.submitTransaction("InitLedger");
+  await contract.submitTransaction('InitLedger');
 
-  console.log("*** Transaction committed successfully");
+  console.log('*** Transaction committed successfully');
 }
+
 
 /**
  * Submit transaction asynchronously, allowing the application to process the smart contract response (e.g. update a UI)
@@ -371,6 +340,16 @@ async function transferAssetAsync(contract: Contract): Promise<void> {
   console.log("*** Transaction committed successfully");
 }
 
+async function getAllAssets(contract: Contract): Promise<void> {
+  console.log('\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger');
+
+  const resultBytes = await contract.evaluateTransaction('GetAllAssets');
+
+  const resultJson = utf8Decoder.decode(resultBytes);
+  const result = JSON.parse(resultJson);
+  console.log('*** Result:', result);
+}
+
 /**
  * submitTransaction() will throw an error containing details of any error responses from the smart contract.
  */
@@ -395,18 +374,25 @@ async function updateNonExistentAsset(contract: Contract): Promise<void> {
 }
 
 /**
- * displayInputParameters() will print the global scope parameters used by the main driver routine.
+ * envOrDefault() will return the value of an environment variable, or a default value if the variable is undefined.
  */
+ function envOrDefault(key: string, defaultValue: string): string {
+  return process.env[key] || defaultValue;
+}
+
+/**
+* displayInputParameters() will print the global scope parameters used by the main driver routine.
+*/
 async function displayInputParameters(): Promise<void> {
   console.log(`channelName:       ${channelName}`);
-    console.log(`chaincodeName:     ${chaincodeName}`);
-    console.log(`mspId:             ${mspId}`);
-    console.log(`cryptoPath:        ${cryptoPath}`);
-    console.log(`keyDirectoryPath:  ${keyDirectoryPath}`);
-    console.log(`certDirectoryPath: ${certDirectoryPath}`);
-    console.log(`tlsCertPath:       ${tlsCertPath}`);
-    console.log(`peerEndpoint:      ${peerEndpoint}`);
-    console.log(`peerHostAlias:     ${peerHostAlias}`);
+  console.log(`chaincodeName:     ${chaincodeName}`);
+  console.log(`mspId:             ${mspId}`);
+  console.log(`cryptoPath:        ${cryptoPath}`);
+  console.log(`keyDirectoryPath:  ${keyDirectoryPath}`);
+  console.log(`certDirectoryPath: ${certDirectoryPath}`);
+  console.log(`tlsCertPath:       ${tlsCertPath}`);
+  console.log(`peerEndpoint:      ${peerEndpoint}`);
+  console.log(`peerHostAlias:     ${peerHostAlias}`);
 }
 
 app.listen(port, () => {
